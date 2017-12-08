6725aad3e7e4ce1c8d29b47167278d804df47c72
PiekarzDev <PiekarzDevStudio@protonmail.com>
2017-12-08 20:52:08 +0100

    add Vuetify FM

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,18 +1,35 @@
 <template>
-<div>
-<h1>Register</h1>
 
-<input type="text" name="email" v-model="email" placeholder="email">
-
-<input type="password" name="password" v-model="password" placeholder="password">
-<br>
-<br>
-<button @click="register">Register</button>
-
-
-</div>
-
-
+ <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Register">
+        <form 
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -21,16 +38,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -38,5 +59,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
